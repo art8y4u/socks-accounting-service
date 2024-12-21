0730195e3121ef6d79b7c30763a595de2aefa32a
vlasotsky <v.lasotsky@gmail.com>
2024-12-21 22:07:34 +0200

    add package-info.java

---
+++ src/main/java/ru/example/socksaccountingservice/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Пакет, содержащий объекты передачи данных.
+ */
+package ru.example.socksaccountingservice.dto;
---
+++ src/main/java/ru/example/socksaccountingservice/service/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Пакет, содержащий объекты сервиса с бизнес-логикой для учета носков.
+ */
+package ru.example.socksaccountingservice.service;
